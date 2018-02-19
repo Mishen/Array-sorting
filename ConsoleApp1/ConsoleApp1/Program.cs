1d2a5c531ec74be09801aa8fc245c8e0a66a8d29
Mishen <mishen888@yandex.ru>
2018-02-19 20:01:04 +0300

    1st

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace SortApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int[] nums = new int[20];
+            Console.WriteLine("Insert twenty numbers");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0} number: ", i + 1);
+                nums[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            // sorting
+            int temp;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+            // output
+            Console.WriteLine("Sorting array");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
